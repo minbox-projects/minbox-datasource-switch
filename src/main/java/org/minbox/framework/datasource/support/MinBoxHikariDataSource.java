e6c639b37e9b638eeceeb3abf75389439dfd8cc6
hengboy <jnyuqy@gmail.com>
2020-07-28 14:31:58 +0800

    Enrich HikariDataSource data source configuration parameters

--- src/main/java/org/minbox/framework/datasource/support/MinBoxHikariDataSource.java
+++ src/main/java/org/minbox/framework/datasource/support/MinBoxHikariDataSource.java
@@ -19,6 +19,13 @@ public class MinBoxHikariDataSource extends HikariDataSource implements MinBoxDa
             this.setJdbcUrl(config.getUrl());
             this.setUsername(config.getUsername());
             this.setPassword(config.getPassword());
+            this.setAutoCommit(config.isAutoCommit());
+            this.setConnectionTimeout(config.getConnectionTimeout());
+            this.setIdleTimeout(config.getIdleTimeout());
+            this.setMinimumIdle(config.getMinimumIdle());
+            this.setMaximumPoolSize(config.getMaxPoolSize());
+            this.setMaxLifetime(config.getMaxLifetime());
+            this.setConnectionTestQuery(config.getConnectionTestQuery());
             config.getProperty().keySet().stream().forEach(param -> this.addDataSourceProperty(param, config.getProperty().get(param)));
         } catch (Exception e) {
             throw new DataSourceSwitchException("Create new Hikari dataSource fail.", e);
